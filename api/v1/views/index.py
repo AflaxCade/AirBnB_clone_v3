1e32e13ea9587ee18441679bd61306c07a8caa95
AflaxCade <116007622+AflaxCade@users.noreply.github.com>
2023-10-27 16:10:10 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Flask route that returns json status response
+Flask route that returns JSON status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
@@ -10,7 +10,7 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def status():
     """
-    function for status route that returns the status
+    Function for the status route that returns the status
     """
     if request.method == 'GET':
         resp = {"status": "OK"}
@@ -20,11 +20,11 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """
-    function to return the count of all class objects
+    Function to return the count of all class objects
     """
     if request.method == 'GET':
         response = {}
-        PLURALS = {
+        plurals = {
             "Amenity": "amenities",
             "City": "cities",
             "Place": "places",
@@ -32,6 +32,6 @@ def stats():
             "State": "states",
             "User": "users"
         }
-        for key, value in PLURALS.items():
+        for key, value in plurals.items():
             response[value] = storage.count(key)
         return jsonify(response)
